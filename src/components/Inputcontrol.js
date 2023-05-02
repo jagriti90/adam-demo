f7d6ad3dc6c612498f70537909a230762c9e6b30
jagriti90 <117734174+jagriti90@users.noreply.github.com>
2023-05-03 02:02:51 +0530

    first commit

--- src/components/Inputcontrol.js
+++ src/components/Inputcontrol.js
@@ -5,8 +5,8 @@ const Inputcontrol = (props) => {
   return (
  <>
  <div className='main_section'>
-    {props.label && <label className=''>{props.label}</label>}
-    <input  className='outline-none out_bar border-white p-5  border-2 border-b-sky-400  text-black hover:text-sky-400 focus:text-black' type='text' {...props}></input>
+    {props.label && <label className='label'>{props.label}</label>}
+    <input   className=' input outline-none out_bar border-white p-5  border-2 border-b-sky-400  text-black hover:text-sky-400 focus:text-black' type='text' {...props}></input>
  </div>
  </>
   )
